c7beabd55cca6fd0947d977d7d5a69402f31173d
Vansil <silvandeboer@gmail.com>
2019-05-31 02:33:05 +0200

    test t-sne new

--- tsne_prep.py
+++ tsne_prep.py
@@ -1,5 +1,6 @@
 import visual
 
-w = visual.WicTsne('output/vua-snli/checkpoints/snli_epoch20.pt',layer=2,device='cuda')
-w.embed()
+w = visual.WicTsne()
+w.set_model('output/vua-snli/checkpoints/snli_epoch20.pt', device='cuda')
+w.embed(layer=2)
 w.save_embeddings('ouput/vua-snli/evaluation/tsne_embeddings_snli_epoch20.pt')
