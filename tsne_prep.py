e048ee9128e8f636abad36283c8a588b5e305dc5
Silvan de Boer <lgpu0219@test3.irc.sara.nl.irc.sara.nl>
2019-05-31 10:51:05 +0200

    bf

--- tsne_prep.py
+++ tsne_prep.py
@@ -1,6 +1,7 @@
 import visual
 
+<<<<<<< Updated upstream
 w = visual.WicTsne()
 w.set_model('output/vua-snli/checkpoints/snli_epoch20.pt', device='cuda')
 w.embed(layer=2)
-w.save_embeddings('ouput/vua-snli/evaluation/tsne_embeddings_snli_epoch20.pt')
+w.save_embeddings('output/vua-snli/evaluation/tsne_embeddings_snli_epoch20.pt')
