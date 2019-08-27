b9ea6c827f6f9a89682c4c9ee96e64286d4be9a2
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-08-28 00:28:15 +0200

    baselines

--- significance.py
+++ significance.py
@@ -89,10 +89,13 @@ if __name__ == "__main__":
     gold = gold_df()
     # wic = wic_df()
 
+    baselines = ('baseline_elmo0', 'baseline_elmo1', 'baseline_elmo2', 'baseline_elmo012')
+    base_stages = ('input',)
     elmo_stages = ('snli', 'vpos', 'vpos-snli', 'vpos-vua-snli', 'vua', 'vua-snli', 'vua-vpos')
     bert_stages = ('snli', 'pos', 'pos-snli', 'pos-vua-snli', 'vua', 'vua-snli', 'pos-vua')
-    models = [(model, stages) for model in ('elmo-2', 'elmo-3') for stages in elmo_stages] + \
-            [(f'bert-{size}-{i}', stages) for size in ('base', 'large') for i in ('0', '1', '2', '3', '4') for stages in bert_stages]
+    models = [(model, stages) for model in baselines for stages in base_stages] + \
+        [(model, stages) for model in ('elmo-2', 'elmo-3') for stages in elmo_stages] + \
+        [(f'bert-{size}-{i}', stages) for size in ('base', 'large') for i in ('0', '1', '2', '3', '4') for stages in bert_stages]
 
     print('mcnemar')
     significance_pivot(models, gold, mcnemar_models,  'results/mcnemar.html', 1.0)
